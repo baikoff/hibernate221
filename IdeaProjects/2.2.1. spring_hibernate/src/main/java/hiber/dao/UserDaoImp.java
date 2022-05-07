03d9f92349c83b506771def087b4dc84f785f534
Artem Baykov <baikoff@bk.ru>
2022-05-07 16:35:24 +0300

    eto bilo bolno

---
+++ IdeaProjects/2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,37 @@
+package hiber.dao;
+
+import hiber.model.Car;
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
+      return query.getResultList();
+   }
+
+   @Override
+   public User getUserByCar(String model, int series) {
+      String query = "from User u where u.car.model= :mod and u.car.series = :ser";
+      TypedQuery<User> query2 = sessionFactory.getCurrentSession().createQuery(query);
+      query2.setParameter("mod",model).setParameter("ser", series);
+      return query2.setMaxResults(1).getSingleResult();
+   }
+}
